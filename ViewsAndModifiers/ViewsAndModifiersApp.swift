dd5ff5cbbb102de85cd5746a4e6b8caaeaa4473b
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-17 21:35:15 +0200

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Sebastien REMY on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
