e626428a76df478187eaee06755d59d805193197
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-18 07:39:38 +0200

    Custom modifiers

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,17 +7,52 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    func body(content: Content) -> some View {
+        VStack(spacing: 0) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+    func watermaked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-        .padding()
-        .background(.red)
-        .padding()
-        .background(.blue)
-        .padding()
-        .background(.green)
-        .padding()
-        .background(.yellow)
+        VStack {
+            Text("Hello, world!").modifier(Title())
+            Text("Hello Seb")
+                .titleStyle()
+                .watermaked(with: "Nice day")
+            Spacer()
+            Color.blue
+                .frame(width: 300, height: 200)
+                .watermaked(with: "Hacking with swift")
+        }
         .onAppear {
             print(type(of: self.body))
         }
